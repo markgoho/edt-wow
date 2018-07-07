0fb21c1b4f9d75a494c5b10fb6a3bffef19b875c
Mark Goho <markgoho@gmail.com>
2018-07-07 12:24:16 -0400

    tweaks for accessibility

--- src/app/search/containers/search/search.component.ts
+++ src/app/search/containers/search/search.component.ts
@@ -1,48 +1,16 @@
-
-import {catchError} from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { SearchService } from '../../search.service';
-
-import { Observable ,  of } from 'rxjs';
-
-
-
-import { Stats } from '../../../models/character-stats';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Items } from '../../../models/character-items';
+import { Stats } from '../../../models/character-stats';
 import { Realm } from '../../../models/realms';
+import { SearchService } from '../../search.service';
 
 @Component({
   selector: 'app-search',
   styleUrls: ['search.component.scss'],
-  template: `
-    <div>
-      <form [formGroup]="form" (ngSubmit)="onSubmit()">
-        <input
-          type="text"
-          placeholder="Character"
-          formControlName="name">
-        <select *ngIf="realms$ | async as realms"
-          formControlName="realm">
-          <option value="" selected>Select a realm</option>
-          <option *ngFor="let realm of realms" [value]="realm.name">{{realm.name}}</option>
-        </select>
-        <button type="submit"
-          [disabled]="form.invalid">Search</button>
-      </form>
-
-      <div class="character-results" *ngIf="stats$">
-        <img (click)="clearResults()" src="/assets/close.png" alt="close button">
-        <app-character-stats
-          [stats]="stats$ | async">
-        </app-character-stats>
-        <app-character-items
-          [items]="items$ | async">
-        </app-character-items>
-      </div>
-
-    </div>
-  `
+  templateUrl: 'search.component.html',
 })
 export class SearchInputComponent implements OnInit {
   form: FormGroup;
@@ -62,18 +30,22 @@ export class SearchInputComponent implements OnInit {
   createForm() {
     this.form = this.fb.group({
       name: ['', Validators.required],
-      realm: ['', Validators.required]
+      realm: ['', Validators.required],
     });
   }
 
   onSubmit() {
     const { name, realm } = this.form.value;
-    this.stats$ = this.search.getStats(name, realm).pipe(catchError(err => {
-      return of({ empty: true });
-    }));
-    this.items$ = this.search.getEquipment(name, realm).pipe(catchError(err => {
-      return of({ empty: true });
-    }));
+    this.stats$ = this.search.getStats(name, realm).pipe(
+      catchError(err => {
+        return of({ empty: true });
+      })
+    );
+    this.items$ = this.search.getEquipment(name, realm).pipe(
+      catchError(err => {
+        return of({ empty: true });
+      })
+    );
   }
 
   clearResults() {
